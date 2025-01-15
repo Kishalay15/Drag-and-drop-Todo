84acc34c68a7d889461b355f1f3cea6d198e71ec
Kishalay15 <kishalaylahiri@gmail.com>
2025-01-16 01:28:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Custom Dragula JS */
+dragula([
+  document.getElementById("to-do"),
+  document.getElementById("doing"),
+  document.getElementById("done"),
+  document.getElementById("trash")
+]);
+
+function addTask() {
+  var inputTask = document.getElementById("taskText").value;
+  document.getElementById("to-do").innerHTML +=
+    "<li class='task'><p>" + inputTask + "</p></li>";
+  document.getElementById("taskText").value = "";
+}
+
+function emptyTrash() {
+  document.getElementById("trash").innerHTML = "";
+}
